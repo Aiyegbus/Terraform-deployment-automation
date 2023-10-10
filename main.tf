46b78728bf42523a979328aad77a0c0d8cc4689f
Aiyegbus <133551804+Aiyegbus@users.noreply.github.com>
2023-10-11 00:24:24 +0100

    new changes to .tf code

--- main.tf
+++ main.tf
@@ -16,29 +16,10 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-1"
-  profile = "my-profile"
+  region = "us-west-1"
 
 }
 
-# creating vpc
-resource "aws_vpc" "tf_auto_vpc" {
-  cidr_block = "10.0.0.0/16"
-}
-
-# creating a subnet
-resource "aws_subnet" "tf_auto_subnet" {
-  vpc_id                  = "aws_vpc.tf_auto_vpc.id"
-  cidr_block              = "10.0.0.0/16"
-  availability_zone       = "us-west-1"
-  map_public_ip_on_launch = true
-}
-
-# creating security group
-resource "aws_security_group" "tf_auto_sg" {
-  name        = "terraform_automation_sg"
-  description = "Security Group for terraform automation"
-}
 
 ## creating ec2 instance (ubuntu )
 resource "aws_instance" "tf_auto_instance" {
